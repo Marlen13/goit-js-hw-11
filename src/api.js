151711d3e53c6b0eba271079270f659d3c9c2c9e
Marlen13 <olenamarinova@gmail.com>
2023-06-05 13:48:37 +0300

    add changes

---
+++ src/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '37030497-adb1b30a9790add5f50421470';
+export async function fetchImages(q, page) {
+  try {
+    const { data } = await axios(
+      `?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=200`
+    );
+      if (data.hits.length === 0) { 
+        
+          Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+    }
+    return data;
+  } catch (error) {
+Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,95 +1,69 @@
-import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(axios);
+import { fetchImages } from './api';
 
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-// // const BASE_URL = 'https://pixabay.com/api/';
-// const API_KEY = '37030497-adb1b30a9790add5f50421470';
-
-// let pageToFetch = 1;
-// let queryToFetch = '';
-
-// async function fetchEvents(q, page) {
-//   try {
-//     const { data } = await axios('', {
-//       param: {
-//         key: API_KEY,
-//         q,
-//         image_type: 'photo',
-//         orientation: 'horizontal',
-//         safesearch: true,
-//         page: pageToFetch,
-//         per_page,
-//       },
-//     });
-//     console.log(data);
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-// }
-// }
+let pageToFetch = 1;
+let queryToFetch = '';
+let lightbox = new SimpleLightbox('.gallery a', {
+});
 
 
-// form.addEventListener("submit", handleSubmit);
-// function handleSubmit(event) {
-//     event.preventDefault();
-//     gallery.innerHTML = "";
-//     const inputValue = event.target.elements.earchQuery.value;
-//     fetchEvents(inputValue, page);
-//     queryToFetch = inputValue;
-//     getEvents(queryToFetch, pageToFetch);
-// }
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    pageToFetch = 1;
+  gallery.innerHTML = '';
+  queryToFetch = event.target.elements.searchQuery.value;
+  fetchImages(queryToFetch, pageToFetch).then(data => createMarkup(data)) ;
+}
 
-// async function getEvents(q, page) {
-//     const data = await fetchEvents(q, page)
-//     .then(data => {
-//         console.log(data);
-//         if (!data || data === []) {
-//                 // allert('Sorry, there are no images matching your search query. Please try again.')
-//                 }
-//     }
+function createMarkup(photo) {
+
+    if (photo === undefined) {
+        loadMoreBtn.classList.add("unvisible");
+        return;
+    }
+            const markup = photo?.hits
+                .map(
+                    ({
+                        webformatURL,
+                        largeImageURL,
+                        tags,
+                        likes,
+                        views,
+                        comments,
+                        downloads,
+                    }) => {
+                        return `<a class="gallery-link" href="${largeImageURL}">
+<img class="gallery-img" src="${webformatURL}" alt="${tags}"></a>
+<div class="img-container">
+<p class="img-info"><b>Likes</b>${likes}</p>
+<p class="img-info">${views}</p>
+<p class="img-info">${comments}</p>
+<p class="img-info">${downloads}</p>
+</div>`;
+        }
+    )
+    .join('');
+    gallery.insertAdjacentHTML('beforeend', markup);
+    lightbox.refresh();
+    if (photo?.hits.length !== 0) {
         
-    // function renderEvents(events) {
-    //             const markup = events
-    //                 .map(
-    //                     ({
-    //                         webformatURL,
-    //                         largeImageURL,
-    //                         tags,
-    //                         likes,
-    //                         views,
-    //                         comments,
-    //                         downloads,
-    //                     }) => {
-    //                         return `<a class="gallery-link" href="${largeImageURL}">
-                // <img class="gallery-img" src="${webformatURL}" alt="${tags}"></a> 
-                // <div class="img-container">
-                // <p class="img-info">${likes}</p>
-                // <p class="img-info">${views}</p>
-                // <p class="img-info">${comments}</p>
-                // <p class="img-info">${downloads}</p>
-                // </div>`;
-                //         }
-                //     )
-//                     .join('');
-//         gallery.insertAdjacentHTML('beforeend', markup);
-//         loadMoreBtn.classList.remove("unvisible")
-// }
-
+        Notiflix.Notify.success(`Hooray! We found ${photo?.totalHits} images.`)    
         
-// loadMoreBtn.addEventListener("click", handleLoadMore);
-                
-// function handleLoadMore() {
-// loadMoreBtn.classList.add("unvisible");
-// pageToFetch += 1;
-// getEvents(queryToFetch, pageToFetch);
-// }
-                    
-                     
+        loadMoreBtn.classList.remove("unvisible")
+    }
+}
+
+loadMoreBtn.addEventListener("click", handleLoadMore);
+
+function handleLoadMore() {
+pageToFetch += 1;
+    fetchImages(queryToFetch, pageToFetch).then(data => createMarkup(data));
+}
